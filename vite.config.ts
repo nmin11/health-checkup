f7a0bdc7d2104f83853b5e41d9a55d2a2b549366
nmin11 <nmin1124@gmail.com>
2025-12-05 21:33:29 +0900

    chore: 모듈 추가 및 설정 파일 정리

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,30 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.candiy.io",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
